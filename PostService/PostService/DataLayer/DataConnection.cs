93af8ef59258513a27bced58470b7b5142022cc5
Som Tiwari <Som.Tiwari@unisys.com>
2019-11-20 05:15:52 +0530

    Updated the swagger.

--- PostService/PostService/DataLayer/DataConnection.cs
+++ PostService/PostService/DataLayer/DataConnection.cs
@@ -1,4 +1,5 @@
-﻿using PostService.Models;
+﻿using MongoDB.Driver;
+using PostService.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,10 @@ namespace PostService.DataLayer
         public List<Post> GetUserPost()
         {
             List<Post> data = new List<Post>();
-
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase("mongodb");
+            var dblist = db.ListCollections().ToList();
             return data;
         }
         public Post AddPost(UserToken userToken)
--- PostService/PostService/Startup.cs
+++ PostService/PostService/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace PostService
 {
@@ -14,8 +17,34 @@ namespace PostService
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AirPortService3_0", Version = "v1" });
+            });
+
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.CustomSchemaIds(x => x.FullName);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +54,20 @@ namespace PostService
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middle-ware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            // Enable middle-ware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "PostService");
             });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
         }
     }
 }
